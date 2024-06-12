95996a21939ee0178b99012c3550183006452261
Arthur.MASSEY <arthurmassey31@gmail.com>
2024-06-12 11:01:26 +0200

    création du php et essai de la connection sans base de données

---
+++ login.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $users = file("users.txt", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    $isAuthenticated = false;
+
+    foreach ($users as $user) {
+        list($stored_username, $stored_hashed_password) = explode(",", $user);
+        if ($username == $stored_username && password_verify($password, $stored_hashed_password)) {
+            $isAuthenticated = true;
+            $_SESSION['username'] = $username;
+            break;
+        }
+    }
+
+    if ($isAuthenticated) {
+        // Redirection vers une page sécurisée
+        header("Location: index.html");
+        exit();
+    } else {
+        // Redirection vers la page d'erreur ou de connexion avec un message d'erreur
+        header("Location: index.html");
+        exit();
+    }
+}
+?>
