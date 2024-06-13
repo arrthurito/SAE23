b2e915290416acb484bec27875096cc1aa9cb1ad
math <mathias.monrouzies.iut@gmail.com>
2024-06-13 21:35:43 +0200

    consult fini

--- consultation.php
+++ consultation.php
@@ -45,7 +45,36 @@
         
         // Display data in an HTML table
         if ($result->num_rows > 0) {
-            echo "<table border='1'>";
+            echo "<table class='data-table' border='1'>";
+            echo "<tr><th>TypeCapt</th><th>Valeurs</th><th>Unites</th><th>Horaires</th><th>Date</th><th>Nomcapteur</th></tr>";
+        
+        
+            while($row = $result->fetch_assoc()) {
+                echo "<tr>";
+                echo "<td>" . $row["TypeCapt"] . "</td>";
+                echo "<td>" . $row["Valeurs"] . "</td>";
+                echo "<td>" . $row["Unites"] . "</td>";
+                echo "<td>" . $row["Horaires"] . "</td>";
+                echo "<td>" . $row["Date"] . "</td>";
+                echo "<td>" . $row["NomCapteur"] . "</td>";
+                echo "</tr>";
+            }
+        
+            echo "</table>";
+        } else {
+            echo "Aucune donnée trouvée";
+        }
+
+        $sql = "SELECT * FROM Mesures m INNER JOIN Capteurs c ON c.NomCapteur = m.NomCapteur WHERE c.TypeCapt='luminosite';";  //;
+        $result = $conn->query($sql);
+        
+        if ($result === false) {
+            die("Erreur dans la requête: " . $conn->error);
+        }
+        
+        // Display data in an HTML table
+        if ($result->num_rows > 0) {
+            echo "<table class='data-table' border='1'>";
             echo "<tr><th>TypeCapt</th><th>Valeurs</th><th>Unites</th><th>Horaires</th><th>Date</th><th>Nomcapteur</th></tr>";
         
         
