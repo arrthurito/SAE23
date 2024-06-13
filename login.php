9448bdf09b4d76de8441df6a324188dd7a57cb8a
Arthur.MASSEY <arthurmassey31@gmail.com>
2024-06-13 16:21:11 +0200

    réalisation de la connection en admin

--- login.php
+++ login.php
@@ -1,30 +1,50 @@
 <?php
 session_start();
 
+// Vérifier si le formulaire a été soumis
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Récupérer le login et le mot de passe depuis le formulaire
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    $users = file("users.txt", FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-    $isAuthenticated = false;
+    // Informations de connexion à la base de données
+    $servername = "localhost";
+    $dbusername = "sae23";
+    $dbpassword = "sae23";
+    $dbname = "sae23";
 
-    foreach ($users as $user) {
-        list($stored_username, $stored_hashed_password) = explode(",", $user);
-        if ($username == $stored_username && password_verify($password, $stored_hashed_password)) {
-            $isAuthenticated = true;
-            $_SESSION['username'] = $username;
-            break;
-        }
+    // Créer une connexion
+    $conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
+
+    // Vérifier la connexion
+    if ($conn->connect_error) {
+        die("La connexion a échoué: " . $conn->connect_error);
     }
 
-    if ($isAuthenticated) {
-        // Redirection vers une page sécurisée
-        header("Location: admin.html");
+    // Préparer la requête SQL pour vérifier l'authentification
+    $sql = "SELECT * FROM Administration WHERE login = '$username' AND mdp = '$password'";
+    $result = $conn->query($sql);
+
+    if ($result === false) {
+        die("Erreur dans la requête: " . $conn->error);
+    }
+
+    // Vérifier si l'utilisateur existe et si le mot de passe est correct
+    if ($result->num_rows > 0) {
+        // Authentification réussie, démarrer la session
+        $_SESSION['username'] = $username;
+        $_SESSION['auth'] = true;
+        header("Location: admin.html"); // Rediriger vers la page sécurisée
         exit();
     } else {
-        // Redirection vers la page d'erreur ou de connexion avec un message d'erreur
-        header("Location: erreur.html");
+        // Authentification échouée, rediriger vers la page d'erreur de connexion
+        $_SESSION = array(); // Réinitialisation du tableau de session
+        session_destroy();   // Destruction de la session
+        header("Location: erreur.html"); // Rediriger vers la page d'erreur de connexion
         exit();
     }
+
+    // Fermer la connexion
+    $conn->close();
 }
 ?>
