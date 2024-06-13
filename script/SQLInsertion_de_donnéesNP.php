36f2798db53e786c56333e7918bfa5fd671938d5
math <mathias.monrouzies.iut@gmail.com>
2024-06-13 15:14:45 +0200

    SQLInsertion_de_donn mise à jour

--- script/SQLInsertion_de_donnéesNP.php
+++ script/SQLInsertion_de_donnéesNP.php
@@ -10,16 +10,36 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Vérifier la connexion
 if ($conn->connect_error) {
     die("La connexion a échoué: " . $conn->connect_error);
-} 
+}
 
-// Requête d'insertion
-$sql = "INSERT INTO Capteurs (NomCapteur, Date, TypeCapt, Unites, Valeurs)
-VALUES ('Capteur Humidité 1', '2024-06-12', 'Humidité', '%', 45.0)";
+// Préparer la requête de sélection
+$sql = "SELECT * FROM Mesures m INNER JOIN Capteurs c ON c.NomCapteur = m.NomCapteur";  //;
+$result = $conn->query($sql);
 
-if ($conn->query($sql) === TRUE) {
-    echo "Nouvel enregistrement créé avec succès";
+if ($result === false) {
+    die("Erreur dans la requête: " . $conn->error);
+}
+
+// Afficher les données dans une table HTML
+if ($result->num_rows > 0) {
+    echo "<table border='1'>";
+    echo "<tr><th>TypeCapt</th><th>Valeurs</th><th>Unites</th><th>Horaires</th><th>Date</th><th>Nomcapteur</th></tr>";
+
+
+    while($row = $result->fetch_assoc()) {
+        echo "<tr>";
+        echo "<td>" . $row["TypeCapt"] . "</td>";
+        echo "<td>" . $row["Valeurs"] . "</td>";
+        echo "<td>" . $row["Unites"] . "</td>";
+        echo "<td>" . $row["Horaires"] . "</td>";
+        echo "<td>" . $row["Date"] . "</td>";
+        echo "<td>" . $row["NomCapteur"] . "</td>";
+        echo "</tr>";
+    }
+
+    echo "</table>";
 } else {
-    echo "Erreur: " . $sql . "<br>" . $conn->error;
+    echo "Aucune donnée trouvée";
 }
 
 // Fermer la connexion
