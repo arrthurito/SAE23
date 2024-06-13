eaf5136768dafb0479f5eb862b1c6f0fb893b027
math <mathias.monrouzies.iut@gmail.com>
2024-06-13 14:06:47 +0200

    erreur constraint supprimer

--- script/recup_donne.sh
+++ script/recup_donne.sh
@@ -2,18 +2,18 @@
 
 #varibale creation/data collection
 #donne=$(mosquitto_sub -h mqtt.iut-blagnac.fr -p 1883 -t AM107/by-room/B105/data -C 1)
-donne=$(cat ./brouillon.json)
+donne=$(cat brouillon.json)
 hum=$(echo "$donne" | jq '.[0].humidity')
 lum=$(echo "$donne" | jq '.[0].illumination')
-salle=$(echo "$donne" | jq '.[1].room')
-bat=$(echo "$donne" | jq '.[1].Building')
-appareil=$(echo "$donne" | jq '.[1].deviceName')
+salle=$(echo "$donne" | jq '.[1].room' | tr -d \")
+bat=$(echo "$donne" | jq '.[1].Building' | tr -d \")
+appareil=$(echo "$donne" | jq '.[1].deviceName' | tr -d \")
 Date=$(date +"%D")
 heure=$(date +"%T")
+echo $appareil | tr -d \"
 
 echo "$hum , $lum , $salle , $bat , $Date , $heure , $appareil"
 
-/opt/lampp/bin/mysql -h"localhost" -u"sae23" -p"sae23" "sae23" << EOF
-INSERT INTO Mesures (Date,Horaires,Valeurs,NomCapteur) VALUES ('$Date','$heure','$hum','$appareil')
-EOF
-#INSERT INTO Mesures (Date,Horaires,Valeurs,NomCapteur) VALUES ('$Date','$heure','$hum','$appareil')
+/opt/lampp/bin/mysql -h"localhost" -u"sae23" -p"sae23" "sae23" -e"
+INSERT INTO Mesures (Date,Horaires,Valeurs,NomCapteur) VALUES ('$Date','$heure','$hum','$appareil');"
+#INSERT INTO Mesures (Date,Horaires,Valeurs,NomCapteur) VALUES ('$Date','$heure','$hum','$appareil');
