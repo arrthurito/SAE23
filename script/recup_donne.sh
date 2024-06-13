c5286dd1dd0c0ee85171773877a0d10e4e450912
math <mathias.monrouzies.iut@gmail.com>
2024-06-13 16:57:00 +0200

    final recup donne

--- script/recup_donne.sh
+++ script/recup_donne.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #varibale creation/data collection
-donne=$(mosquitto_sub -h mqtt.iut-blagnac.fr -p 1883 -t AM107/by-room/B105/data -C 1)
+donne=$(mosquitto_sub -h mqtt.iut-blagnac.fr -p 1883 -t AM107/by-room/$1/data -C 1)
 hum=$(echo "$donne" | jq '.[0].humidity')
 lum=$(echo "$donne" | jq '.[0].illumination')
 appareil=$(echo "$donne" | jq '.[1].deviceName' | tr -d \")
@@ -11,6 +11,6 @@ heure=$(date +"%T")
 echo "$hum , $lum , $salle , $bat , $Date , $heure , $appareil"
 
 /opt/lampp/bin/mysql -h"localhost" -u"sae23" -p"sae23" "sae23" -e"
-INSERT INTO Mesures (Date,Horaires,Valeurs,NomCapteur,TypeMes) VALUES ('$Date','$heure','$lum','$appareil','luminosite');
-INSERT INTO Mesures (Date,Horaires,Valeurs,NomCapteur,TypeMes) VALUES ('$Date','$heure','$hum','$appareil','humidite');"
+INSERT INTO Mesures (Date,Horaires,Valeurs,NomCapteur) VALUES ('$Date','$heure','$lum','$appareil\lum');
+INSERT INTO Mesures (Date,Horaires,Valeurs,NomCapteur) VALUES ('$Date','$heure','$hum','$appareil\hum');"
 #INSERT INTO Mesures (Date,Horaires,Valeurs,NomCapteur) VALUES ('$Date','$heure','$hum','$appareil');
